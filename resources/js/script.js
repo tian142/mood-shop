24a6f70a90dfd30006bd7d2fef9d84ef9c97d6d7
Tian Qiu <tian.qiu142@gmail.com>
2020-09-17 14:45:18 -0700

    almost done

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,31 @@
 import data from './data.js';
-const itemList = document.getElementById('item-list');
 
 const itemsContainer = document.getElementById('items');
+const itemList = document.getElementById('item-list');
+const cartQty = document.getElementById('cart-qty');
+const cartTotal = document.getElementById('cart-total');
+const cart = [];
+
+itemList.addEventListener('click', (e) => {
+    // console.log('clicked');
+    // console.log(e.target);
+    if (e.target && e.target.classList.contains('removeBtn')) {
+        const name = e.target.dataset.name; //named in the html creation in showItem
+        // console.log(name);
+        removeItem(name);
+        // if (cart.length === 0) {
+        //     itemList.removeAttribute();
+        // }
+        // console.log(cart);
+    } else if (e.target && e.target.classList.contains('addOne')) {
+        const name = e.target.dataset.name;
+        addItem(name);
+    } else if (e.target && e.target.classList.contains('minusOne')) {
+        const name = e.target.dataset.name;
+        removeItem(name, 1);
+    }
+    showItems();
+});
 
 data.forEach(function (item) {
     let newDiv = document.createElement('div');
@@ -29,8 +53,6 @@ data.forEach(function (item) {
     itemsContainer.appendChild(newDiv);
 });
 
-const cart = [];
-
 const addItem = (name, price) => {
     for (let item in cart) {
         if (cart[item].name === name) {
@@ -49,6 +71,7 @@ const removeItem = (name, qty = 0) => {
                 cart[item].qty -= qty;
             }
             if (cart[item].qty < 1 || qty === 0) cart.splice(name, 1);
+            showItems();
             return;
         }
     }
@@ -57,13 +80,32 @@ const removeItem = (name, qty = 0) => {
 const showItems = () => {
     const totalQty = getTotalQty();
     const totalPrice = getTotalPrice();
+    cartQty.innerHTML = `You have ${totalQty} items in your cart`;
+    cartTotal.innerHTML = `Your cart's total is $${totalPrice}`;
+    let itemStr = '';
     for (let item of cart) {
-        console.log(`-${item.name} $${item.price} x ${item.qty}`);
+        // console.log(`-${item.name} $${item.price} x ${item.qty}`);
+        const { name, price, qty } = item;
+        itemStr += `<li> ${name} 
+        $${price} x ${qty} = 
+        $${(price * qty).toFixed(
+            2
+        )} <button class="removeBtn" data-name="${name}">Remove<button>
+        <button class="addOne" data-name="${name}">+</button>
+        <button class="minusOne" data-name="${name}">-</button>
+        </li>
+        
+        `;
+        itemList.innerHTML = itemStr;
     }
-    console.log(
-        `you have ${totalQty} items in your cart, the total price is: $${totalPrice}`
-    );
 };
+const allItemsButton = Array.from(document.querySelectorAll('button'));
+allItemsButton.forEach((elt) =>
+    elt.addEventListener('click', () => {
+        addItem(elt.getAttribute('id'), elt.getAttribute('data-price'));
+        showItems();
+    })
+);
 
 const getTotalQty = () => {
     let totalQty = 0;
@@ -81,19 +123,4 @@ const getTotalPrice = () => {
     return totalPrice.toFixed(2);
 };
 
-addItem('beef', '2.55');
-addItem('boots', '49.99');
-addItem('boots', '49.99');
-addItem('boots', '49.99');
-// addItem('boots', '49.99');
-// addItem('boots', '49.99');
-// addItem('boots', '49.99');
-// addItem('joe', '0.1');
-
 showItems();
-removeItem('beef', 1);
-removeItem('boots');
-
-showItems();
-
-itemList.innerHTML = '<li> Hello World</li>';
