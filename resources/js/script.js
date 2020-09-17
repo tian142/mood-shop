ae5ee7307009fd4ddd891ab76f0456eb302a608b
Tian Qiu <tian.qiu142@gmail.com>
2020-09-17 11:39:49 -0700

    added notes

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,51 +1,13 @@
 import data from './data.js';
+const itemList = document.getElementById('item-list');
 
 const itemsContainer = document.getElementById('items');
-
-// the length of our data determines how many times this loop goes around
-// for (let i = 0; i < data.length; ++i) {
-//     // create a new div element and give it a class name
-//     let newDiv = document.createElement('div');
-//     newDiv.className = 'item';
-
-//     // create an image element
-//     let img = document.createElement('img');
-//     // this will change each time we go through the loop. Can you explain why?
-//     img.src = data[i].image;
-//     img.width = 300;
-//     img.height = 300;
-
-//     // Add the image to the div
-//     newDiv.appendChild(img);
-//     // put new div inside items container
-//     itemsContainer.appendChild(newDiv);
-
-//     let imageDesc = document.createElement('p');
-//     imageDesc.innerText = data[i].name;
-//     newDiv.appendChild(imageDesc);
-
-//     let price = document.createElement('p');
-//     price.innerText = data[i].price;
-//     newDiv.appendChild(price);
-//     // imageTitle.appendChild;
-//     let button = document.createElement('button');
-//     button.id = data[i].name;
-//     button.dataset.price = data[i].price;
-//     button.innerHTML = 'Add to Cart';
-//     newDiv.appendChild(button);
-
-//     itemsContainer.appendChild(newDiv);
-// }
-
-// for(item of data){
-//     console.log item.id
-// }
 
 data.forEach(function (item) {
     let newDiv = document.createElement('div');
     newDiv.className = 'item';
     let img = document.createElement('img');
-    img.src = item.image;
+    // img.src = item.image;
     img.width = 300;
     img.height = 300;
     newDiv.appendChild(img);
@@ -66,3 +28,72 @@ data.forEach(function (item) {
 
     itemsContainer.appendChild(newDiv);
 });
+
+const cart = [];
+
+const addItem = (name, price) => {
+    for (let item in cart) {
+        if (cart[item].name === name) {
+            cart[item].qty += 1;
+            return;
+        }
+    }
+    const item = { name, price, qty: 1 };
+    cart.push(item);
+};
+
+const removeItem = (name, qty = 0) => {
+    for (let item in cart) {
+        if (cart[item].name === name) {
+            if (qty > 0) {
+                cart[item].qty -= qty;
+            }
+            if (cart[item].qty < 1 || qty === 0) cart.splice(name, 1);
+            return;
+        }
+    }
+};
+
+const showItems = () => {
+    const totalQty = getTotalQty();
+    const totalPrice = getTotalPrice();
+    for (let item of cart) {
+        console.log(`-${item.name} $${item.price} x ${item.qty}`);
+    }
+    console.log(
+        `you have ${totalQty} items in your cart, the total price is: $${totalPrice}`
+    );
+};
+
+const getTotalQty = () => {
+    let totalQty = 0;
+    for (let item of cart) {
+        totalQty += item.qty;
+    }
+    return totalQty;
+};
+
+const getTotalPrice = () => {
+    let totalPrice = 0;
+    for (let item of cart) {
+        totalPrice += item.price * item.qty;
+    }
+    return totalPrice.toFixed(2);
+};
+
+addItem('beef', '2.55');
+addItem('boots', '49.99');
+addItem('boots', '49.99');
+addItem('boots', '49.99');
+// addItem('boots', '49.99');
+// addItem('boots', '49.99');
+// addItem('boots', '49.99');
+// addItem('joe', '0.1');
+
+showItems();
+removeItem('beef', 1);
+removeItem('boots');
+
+showItems();
+
+itemList.innerHTML = '<li> Hello World</li>';
